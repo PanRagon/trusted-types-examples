652da17604e69ca491acdc0b9848f7170c2d8206
Christian Iversen <thebigecko@gmail.com>
2022-06-16 13:05:41 +0200

    Typo

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,7 +3,7 @@ import { reviews } from '../db/reviews';
 export const Reviews = () => {
   return (
     <div>
-      <h2>Selection of my reviews</h2>
+      <h2>Selection of our reviews</h2>
       {reviews.map((review, i) => (
         <div className="review" key={i}>
           <h4 dangerouslySetInnerHTML={{ __html: review.title }}></h4>
